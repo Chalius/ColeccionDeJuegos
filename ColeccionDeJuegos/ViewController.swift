2b86a8aaef8fcd7d86965a14b09cc96be3f730fd
Gonzalo <gonzalo.quispe.f@tecsup.edu.pe>
2020-06-14 11:09:22 -0700

    tarea del lab9 terminada, falta implementar investigación y aplicación

--- ColeccionDeJuegos/ViewController.swift
+++ ColeccionDeJuegos/ViewController.swift
@@ -2,6 +2,9 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    @IBOutlet weak var tableView: UITableView!
+    var juegos : [Juego] = []
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return juegos.count
@@ -11,7 +14,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let cell = UITableViewCell()
         let juego = juegos[indexPath.row]
         cell.textLabel?.text = juego.titulo
-        cell.imageView?.image = UIImage(data: (juego.imagen!))
+        if(juego.imagen != nil){
+            cell.imageView?.image = UIImage(data: (juego.imagen!))
+        }
+        
         return cell
     }
     
@@ -25,12 +31,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         siguienteVC.juego = sender as? Juego
     }
     
-    
-    
-    @IBOutlet weak var tableView: UITableView!
-    var juegos : [Juego] = []
-    
-    
     override func viewWillAppear(_ animated: Bool) {
         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
         do{
@@ -43,11 +43,45 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     
+    // implementando funcionalidad de borrar elementos de la tabla en la misma vista:
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            //arregloNumeros.remove(at:indexPath.row)
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            
+            context.delete(juegos[indexPath.row])
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            
+            tableView.reloadData()
+        }else if editingStyle == .insert{
+            
+        }
+    }
+    
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        true
+    }
+    
+    // función para mover registros de la tabla:
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let objetoMovido = self.juegos[sourceIndexPath.row]
+        juegos.remove(at: sourceIndexPath.row)
+        juegos.insert(objetoMovido, at: destinationIndexPath.row)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        true
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
         
+        tableView.isEditing = true
     }
 
 
