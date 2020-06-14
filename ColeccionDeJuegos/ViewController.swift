87a6127fb1371d86294bd79cd1fdc1a95fb3f6a8
Gonzalo <gonzalo.quispe.f@tecsup.edu.pe>
2020-06-13 21:44:56 -0700

    Initial Commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by Gonzalo Quispe Fernandez on 6/13/20.
+//  Copyright © 2020 Gonzalo Quispe Fernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
