6d292c730ae3a235c9250920d54738d8aecc8bf8
Gonzalo <gonzalo.quispe.f@tecsup.edu.pe>
2020-06-14 09:49:48 -0700

    se muestran las imagenes en table view principal

--- ColeccionDeJuegos/ViewController.swift
+++ ColeccionDeJuegos/ViewController.swift
@@ -1,18 +1,43 @@
-//
-//  ViewController.swift
-//  ColeccionDeJuegos
-//
-//  Created by Gonzalo Quispe Fernandez on 6/13/20.
-//  Copyright © 2020 Gonzalo Quispe Fernandez. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return juegos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let juego = juegos[indexPath.row]
+        cell.textLabel?.text = juego.titulo
+        cell.imageView?.image = UIImage(data: (juego.imagen!))
+        return cell
+    }
+    
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    var juegos : [Juego] = []
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do{
+            try juegos = context.fetch(Juego.fetchRequest())
+            tableView.reloadData()
+        }catch{
+            
+        }
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+        
     }
 
 
